7c70d0a3e8efec6c07d1d32324c709f1c50fac7b
gaspard <gaspard@osef.fr>
2024-12-27 11:12:41 +0100

    Add netmask information

--- ifnetshow.c
+++ ifnetshow.c
@@ -8,8 +8,15 @@ int main() {
 	getifaddrs(&ifaddr);
 	while(ifaddr){
 		if (ifaddr->ifa_addr && ifaddr->ifa_addr->sa_family == AF_INET) {
-			printf("%s: %s\n", ifaddr->ifa_name, inet_ntoa(((struct sockaddr_in *)ifaddr->ifa_addr)->sin_addr));
-		}
+			char ip[INET_ADDRSTRLEN];
+			char netmask[INET_ADDRSTRLEN];
+
+			// Convert the IP address to string
+			inet_ntop(AF_INET, &((struct sockaddr_in *)ifaddr->ifa_addr)->sin_addr, ip, INET_ADDRSTRLEN);
+			// Convert the netmask to string
+			inet_ntop(AF_INET, &((struct sockaddr_in *)ifaddr->ifa_netmask)->sin_addr, netmask, INET_ADDRSTRLEN);
+			printf("Int %s: IP=%s, Netmask=%s\n", ifaddr->ifa_name, ip , netmask);
+		}            
 		ifaddr = ifaddr->ifa_next;
 	}
 	return 0;
